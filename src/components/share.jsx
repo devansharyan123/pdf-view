da0664e0cb6eef7b69f890f9971a2cdfe91bc00e
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2024-11-12 00:41:11 +0530

    style fix

--- src/components/share.jsx
+++ src/components/share.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 
 const PDFViewer = () => {
-  // URL of the PDF in your S3 bucket
+  
   const s3PDFUrl = 'https://kyra-dating-coach.s3.ap-southeast-2.amazonaws.com/Devansh_Aryan.pdf';
-
+  
+ 
   const [showPDF, setShowPDF] = useState(false);
 
   const togglePDFView = () => {
-    setShowPDF(!showPDF);
+    setShowPDF(!showPDF); 
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       {!showPDF ? (
         <>
           <button
             onClick={togglePDFView}
-            className="px-6 py-3 mt-4 text-lg font-semibold text-white bg-blue-500 rounded-md transition duration-300 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            className="px-6 py-3 mt-4 text-lg font-semibold text-white transition duration-300 bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             View PDF
           </button>
@@ -25,20 +26,19 @@ const PDFViewer = () => {
           </p>
         </>
       ) : (
-        <div className="relative w-full max-w-4xl h-[60vh] md:h-[80vh] lg:h-[90vh] xl:h-[95vh]">
-          {/* Close Button */}
+        <div className="relative w-full h-screen">
+         
           <button
             onClick={togglePDFView}
-            className="absolute top-4 right-4 text-white bg-transparent rounded-full p-2 hover:bg-transparent focus:outline-none focus:ring-4 focus:ring-red-300 text-xl sm:text-2xl md:text-3xl"
+            className="absolute top-20 right-5 text-white bg-transparent rounded-full p-2 hover:bg-transparent focus:outline-none focus:ring-4 focus:ring-red-300"
           >
-            &#10005; {/* Unicode for "X" symbol */}
+            &#10005;
           </button>
-
-          {/* PDF Viewer */}
+          
           <iframe
             src={s3PDFUrl}
             title="PDF Viewer"
-            className="w-full h-full border-0 rounded"
+            className="w-full h-full border-0"
           />
         </div>
       )}
